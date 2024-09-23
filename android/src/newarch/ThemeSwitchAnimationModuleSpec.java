e5593e1077c9516c3297e3544d439afdd0e50273
Qinlian1 <168181884+Qinlian1@users.noreply.github.com>
2024-09-23 15:13:58 +0800

    feat: support Harmony NEXT (#3)
    Co-authored-by: qinlianao <qinlianao@h-partners.com>

--- android/src/newarch/ThemeSwitchAnimationModuleSpec.java
+++
@@ -1,9 +0,0 @@
-package com.themeswitchanimation;
-
-import com.facebook.react.bridge.ReactApplicationContext;
-
-abstract class ThemeSwitchAnimationModuleSpec extends NativeThemeSwitchAnimationModuleSpec {
-  ThemeSwitchAnimationModuleSpec(ReactApplicationContext context) {
-    super(context);
-  }
-}
